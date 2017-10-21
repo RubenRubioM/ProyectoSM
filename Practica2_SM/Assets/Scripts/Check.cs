1c192b36bdbe91cc8921a8fdc74f9b229449506e
Ruben <rubensipala@gmail.com>
2017-10-21 20:17:13 +0200

    Primer dia

---
+++ Practica2_SM/Assets/Scripts/Check.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Check : MonoBehaviour {
+
+    public GameObject good_panel,bad_panel;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnMouseDown() {
+        if (gameObject.CompareTag("Plastic")) {
+            if (bad_panel.activeInHierarchy) {
+                good_panel.SetActive(true);
+            } else {
+                bad_panel.SetActive(false);
+                good_panel.SetActive(true);
+            }
+            
+        } else {
+            bad_panel.SetActive(true);
+        }
+    }
+}
---
+++ Practica2_SM/Assets/Scripts/KidMovement.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class KidMovement : MonoBehaviour {
+
+    [SerializeField]
+    private float speed;
+
+    private Vector2  positionToMove; //Variable que guarda la posicion donde has tocado
+
+   	void Start () {
+		
+	}
+
+
+    void Update () {
+        
+        
+        if(Input.touchCount>0 && Input.GetTouch(0).phase == TouchPhase.Began) {
+            foreach (Touch touch in Input.touches) {
+
+                positionToMove = Camera.main.ScreenToWorldPoint(touch.position);
+                Debug.Log("Posicion donde has tocado: " + positionToMove);
+                Debug.Log("Posicion del personaje:" + transform.position);
+
+            }
+            Vector2 distance = positionToMove - (Vector2)transform.position;
+            transform.position = positionToMove;
+        }
+        
+        
+        
+
+    }
+
+    /*
+    private void MoveTo(Vector2 distance) {
+       
+
+        Debug.Log(distance);
+
+        while ((Vector2) transform.position!=positionToMove) {
+
+            distance = positionToMove - (Vector2)transform.position;
+            transform.Translate(speed,speed,0);
+
+        }
+        
+    }
+    */
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+
+        if (collision.gameObject.CompareTag("Raincoat")) {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+        }
+    }
+}
