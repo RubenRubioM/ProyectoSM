2d2b853fb66bde88214b44f25136a7361309dd37
Ruben <rubensipala@gmail.com>
2017-10-27 21:37:45 +0200

    movimiento casi on point

--- Practica2_SM/Assets/Scripts/KidMovement.cs
+++ Practica2_SM/Assets/Scripts/KidMovement.cs
@@ -5,29 +5,30 @@ using UnityEngine.SceneManagement;
 
 public class KidMovement : MonoBehaviour {
 
+    public GameObject circuloPrefab;
+
     [SerializeField]
     private float speed;
     private bool moving;
-    private Vector2  positionToMove; //Variable que guarda la posicion donde has tocado
+    private Vector2 positionToMove; //Variable que guarda la posicion donde has tocado
     private Vector2 distance;
 
-   	void Start () {
-		
-	}
 
-
-    void Update () {
+    void Update() {
 
         if (Input.GetMouseButtonDown(0)) {
             positionToMove = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            Debug.Log(positionToMove);
+            
 
             distance = new Vector2(positionToMove.x - transform.position.x, positionToMove.y - transform.position.y);
+            circuloPrefab.transform.position = positionToMove;
+            
             moving = true;
 
         }
 
-        if (distance.x < 0) {
+        
+        if (moving == true && distance.x < 0) {
             if (distance.y < 0) {
                 transform.Translate(-speed * Time.deltaTime, -speed * Time.deltaTime, 0);
             }
@@ -39,7 +40,7 @@ public class KidMovement : MonoBehaviour {
             }
         }
 
-        if (distance.x > 0) {
+        if (moving == true && distance.x > 0) {
             if (distance.y < 0) {
                 transform.Translate(speed * Time.deltaTime, -speed * Time.deltaTime, 0);
             }
@@ -50,16 +51,29 @@ public class KidMovement : MonoBehaviour {
                 transform.Translate(speed * Time.deltaTime, 0, 0);
             }
         }
+
+
         distance = new Vector2(positionToMove.x - transform.position.x, positionToMove.y - transform.position.y);
 
-
+        
     }
-
 
     private void OnTriggerEnter2D(Collider2D collision) {
 
         if (collision.gameObject.CompareTag("Raincoat")) {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+        if (collision.gameObject.CompareTag("circulo")) {
+            moving = false;
+            
         }
     }
+
+ 
 }
+
+
+
+
+   
+
