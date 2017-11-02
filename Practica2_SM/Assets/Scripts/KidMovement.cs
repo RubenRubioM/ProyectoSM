76b76b80750ae623a72986bf8e9b608c3d0df605
Ruben <rubensipala@gmail.com>
2017-11-02 23:48:30 +0100

    movimiento done

--- Practica2_SM/Assets/Scripts/KidMovement.cs
+++ Practica2_SM/Assets/Scripts/KidMovement.cs
@@ -5,56 +5,25 @@ using UnityEngine.SceneManagement;
 
 public class KidMovement : MonoBehaviour {
 
-    public GameObject circuloPrefab;
 
     [SerializeField]
     private float speed;
-    private bool moving;
     private Vector2 positionToMove; //Variable que guarda la posicion donde has tocado
-    private Vector2 distance;
+    
 
+    private void Start() {
+        positionToMove = transform.position;
+    }
 
     void Update() {
 
         if (Input.GetMouseButtonDown(0)) {
             positionToMove = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            
-
-            distance = new Vector2(positionToMove.x - transform.position.x, positionToMove.y - transform.position.y);
-            circuloPrefab.transform.position = positionToMove;
-            
-            moving = true;
 
         }
 
-        
-        if (moving == true && distance.x < 0) {
-            if (distance.y < 0) {
-                transform.Translate(-speed * Time.deltaTime, -speed * Time.deltaTime, 0);
-            }
-            if (distance.y > 0) {
-                transform.Translate(-speed * Time.deltaTime, speed * Time.deltaTime, 0);
-            }
-            if (distance.y == 0) {
-                transform.Translate(-speed * Time.deltaTime, 0, 0);
-            }
-        }
-
-        if (moving == true && distance.x > 0) {
-            if (distance.y < 0) {
-                transform.Translate(speed * Time.deltaTime, -speed * Time.deltaTime, 0);
-            }
-            if (distance.y > 0) {
-                transform.Translate(speed * Time.deltaTime, speed * Time.deltaTime, 0);
-            }
-            if (distance.y == 0) {
-                transform.Translate(speed * Time.deltaTime, 0, 0);
-            }
-        }
-
-
-        distance = new Vector2(positionToMove.x - transform.position.x, positionToMove.y - transform.position.y);
-
+        transform.position = Vector2.MoveTowards(transform.position, positionToMove, speed * Time.deltaTime);
+      
         
     }
 
@@ -63,10 +32,7 @@ public class KidMovement : MonoBehaviour {
         if (collision.gameObject.CompareTag("Raincoat")) {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
-        if (collision.gameObject.CompareTag("circulo")) {
-            moving = false;
-            
-        }
+       
     }
 
  
